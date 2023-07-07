ee2696456cc18c4b08d29b1da6ce907a84f5c84b
endangmuslihat <endangmuslihat73@gmail.com>
2023-07-07 15:51:18 +0700

    menambahkan image pada html dan menghapus perintah di js

--- js/script.js
+++ js/script.js
@@ -23,21 +23,11 @@ document.querySelector("#shopping-cart-button").onclick = (e) => {
   e.preventDefault();
 };
 // klik diluar sidebar untuk menghilangkan nav
-const hm = document.querySelector("#hamburger-menu");
-const sb = document.querySelector("#search-button");
-const sc = document.querySelector("#shopping-cart-button");
+const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
-  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
-  }
-
-  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-    searchForm.classList.remove("active");
-  }
-
-  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
-    shoppingCart.classList.remove("active");
   }
 });
 
