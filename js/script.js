03458158c8c87ebd8dc9bf660e745d3c7bdacfe7
coorporate <muslihatendang1973@gmail.com>
2024-07-09 01:02:30 +0700

    merubah menu navbar

--- js/script.js
+++ js/script.js
@@ -54,3 +54,97 @@ window.onclick = (e) => {
     itemDetailModal.style.display = "none";
   }
 };
+
+// hubungi kami
+const form = document.querySelector("form");
+const nama = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+  const bodyMessage = `Nama Lengkap : ${name.value}<br> Email: ${email.value}<br> Nomor Telepon: ${phone.value}<br> Pesan: ${mess.value}<br>`;
+
+  Email.send({
+    SecureToken: "3ccdb8f8-58fc-4104-aa36-eca5d3a02b8c",
+    // Host: "smtp.elasticemail.com",
+    // Username: "cvamandakaryapribadi@gmail.com",
+    // Password: "99C7C610500A57510D26357615A823139C17",
+    To: "cvamandakaryapribadi@gmail.com",
+    From: "cvamandakaryapribadi@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage,
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Berhasil!",
+        text: "Pesan berhasil terkirim!",
+        // text: "Message sent successfully!",
+        icon: "success",
+      });
+    }
+  });
+}
+
+function checkInputs() {
+  const items = document.querySelectorAll(".item");
+
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+
+    if (items[1].value != "") {
+      checkEmail();
+    }
+
+    items[1].addEventListener("keyup", () => {
+      checkEmail();
+    });
+
+    item.addEventListener("keyup", () => {
+      if (item.value != "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+
+function checkEmail() {
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+  const errorTextEmail = document.querySelector(".error-text.email");
+
+  if (!email.value.match(emailRegex)) {
+    email.classList.add("error");
+    email.parentElement.classList.add("error");
+
+    if (email.value != "") {
+      errorTextEmail.innerText = "Enter a valid email address";
+    } else {
+      errorTextEmail.innerText = "Email Adress can't be blank";
+    }
+  } else {
+    email.classList.remove("error");
+    email.parentElement.classList.remove("error");
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+
+  if (!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")) {
+    sendEmail();
+    // console.log("OK");
+
+    // menghilangkan isi form
+    form.reset();
+    return false;
+  }
+});
